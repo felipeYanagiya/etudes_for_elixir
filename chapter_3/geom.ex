06bda07c2769b913255c176f288f65410bc8f61f
Felipe Yanagiya <felipe.yanagiya@concrete.com.br>
2017-02-16 19:24:04 -0200

    3.4

--- chapter_3/geom.ex
+++ chapter_3/geom.ex
@@ -1,15 +1,18 @@
 defmodule Geom do
   @moduledoc """
-  Area/2 function returns calculation of retangule area
-  By default, we have a length = 1 and width = 1 retangule
+  Area/1 function returns calculation of geometry form area
   """
-  def area(which, a, b) do
+  defp area(which, a, b) do
     case which do
       :rectangle when a > 0 and b > 0 -> a * b
       :triangle when a > 0 and b > 0 -> a * b / 2
       :ellipse when a > 0 and b > 0 -> :math.pi * a * b
       _ -> 0
     end
+  end
+
+  def area({shape, number1, number2}) do
+    area shape, number1, number2
   end
 
 end
