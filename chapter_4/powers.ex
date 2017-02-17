396b8f4653e800874dc96277a98f322653f784fc
Felipe Yanagiya <felipe.yanagiya@concrete.com.br>
2017-02-17 19:21:57 -0200

    4.4

--- chapter_4/powers.ex
+++ chapter_4/powers.ex
@@ -1,12 +1,19 @@
-import Kernel, except: [raise: 2]
+import Kernel, except: [raise: 2, raise: 3]
 
 defmodule Powers do
   def raise(base, exp) do
     cond do
       exp == 0 -> 1
       base == 1 -> base
-      exp > 0 -> base * raise(base, exp - 1)
+      exp > 0 -> raise(base, exp, 1)
       exp < 0 -> 1 / raise(base, -exp)
+    end
+  end
+
+  def raise(base, exp, acc) do
+    cond do
+      exp == 0 -> acc
+      exp > 0 -> raise(base, exp - 1, base * acc)
     end
   end
 end
