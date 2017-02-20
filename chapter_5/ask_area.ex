cd6da81d90adc1631240708828912f016319b754
Felipe Yanagiya <felipe.yanagiya@concrete.com.br>
2017-02-20 19:01:16 -0300

    5.2

--- chapter_5/ask_area.ex
+++ chapter_5/ask_area.ex
@@ -9,38 +9,38 @@ defmodule AskArea do
   end
 
   defp get_number(entry) do
-    String.to_integer String.strip entry, 10
+    cond do
+      Regex.match?(~r/^[1-9]([0-9]+)?$/, entry) -> String.to_integer String.strip entry, 10
+      Regex.match?(~r/^\d\.([0-9]+)?$/, entry) -> String.to_float String.strip entry, 10
+      true -> :error
+    end
   end
 
   defp calculate(shape, dim1, dim2) do
     cond do
       dim1 <= 0 or dim2 <= 0 -> IO.puts "Both numbers must be greater than or equal to zero."
-      true -> Geom.area {shape,dim1, dim2}
+      true -> Geom.area {shape, dim1, dim2}
     end
   end
 
-  def get_first_number(shape) do
-    cond do
-      shape == :rectangle -> get_number IO.gets "Enter width > "
-      shape == :triangle -> get_number IO.gets "Enter base > "
-      shape == :ellipse -> get_number IO.gets "Enter major radius > "
-      true -> :ok
-    end
-  end
-
-  defp get_second_number(shape) do
-    cond do
-      shape == :rectangle -> get_number IO.gets "Enter height > "
-      shape == :triangle -> get_number IO.gets "Enter height > "
-      shape == :ellipse -> get_number IO.gets "Enter minor radius > "
-      true -> :ok
-    end
+  defp get_dimensions(prompt1, prompt2) do
+    num1 =  get_number IO.gets prompt1
+    num2 = get_number IO.gets prompt2
+    {num1, num2}
   end
 
   def area() do
     shape = char_to_shape IO.gets "R)ectangle, T)riangle, or E)llipse: "
-    dim1 = get_first_number shape
-    dim2 = get_second_number shape
-    calculate shape, dim1, dim2
+    {num1, num2} =
+
+    cond do
+      shape == :rectangle -> get_dimensions "Enter height > ", "Enter height > "
+      shape == :triangle -> get_dimensions "Enter base > ", "Enter height > "
+      shape == :ellipse -> get_dimensions "Enter major radius > ", "Enter minor radius > "
+      shape == :unknown -> IO.puts "bla"
+      true -> :err
+    end
+
+    calculate shape, num1, num2
   end
 end
